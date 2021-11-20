71ae477c56ac885dfe48e7e577653708e7a908dd
h0p3zZ <danielwimmer08012003@gmail.com>
2021-11-20 01:48:18 +0100

    added file read

--- go.mod
+++ go.mod
@@ -1,3 +1,12 @@
 module github.com/h0p3zZ/InternetPLS
 
 go 1.17
+
+require (
+	github.com/go-ping/ping v0.0.0-20211014180314-6e2b003bffdd //indirect
+	github.com/google/uuid v1.3.0 // indirect
+	golang.org/x/net v0.0.0-20211118161319-6a13c67c3ce4 // indirect
+	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
+	golang.org/x/sys v0.0.0-20211117180635-dee7805ff2e1 // indirect
+	golang.org/x/time v0.0.0-20211116232009-f0f3c7e86c11 // indirect
+)
--- main.go
+++ main.go
@@ -2,10 +2,25 @@ package main
 
 import (
 	"fmt"
-	"net/http"
+	"net"
+	"os"
+	"time"
 )
 
 func main() {
-	fmt.Printf("hello, world\n")
-	fmt.Print(http.Get("https://google.com"))
+	b, err := os.ReadFile("./file.txt")
+	if err != nil {
+		fmt.Println("File not found!")
+	}
+	fmt.Println(string(b))
+	for {
+		start := time.Now()
+		_, err := net.LookupIP("8.8.8.8")
+		if err != nil {
+			fmt.Println("Connection timed out")
+		}
+		end := time.Now()
+		fmt.Println(end.Sub(start))
+		time.Sleep(1 * time.Second)
+	}
 }
