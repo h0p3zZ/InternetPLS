a16b18db16345f40547e09f5c8d6b5006dcae897
h0p3zZ <danielwimmer08012003@gmail.com>
2021-11-26 18:49:26 +0100

    implemented go-pinger

--- go.mod
+++ go.mod
@@ -5,8 +5,8 @@ go 1.17
 require (
 	github.com/go-ping/ping v0.0.0-20211014180314-6e2b003bffdd //indirect
 	github.com/google/uuid v1.3.0 // indirect
-	golang.org/x/net v0.0.0-20211118161319-6a13c67c3ce4 // indirect
+	golang.org/x/net v0.0.0-20211123203042-d83791d6bcd9 // indirect
 	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
-	golang.org/x/sys v0.0.0-20211117180635-dee7805ff2e1 // indirect
+	golang.org/x/sys v0.0.0-20211124211545-fe61309f8881 // indirect
 	golang.org/x/time v0.0.0-20211116232009-f0f3c7e86c11 // indirect
 )
--- main.go
+++ main.go
@@ -5,10 +5,11 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
-	"net"
 	"net/http"
 	"os"
 	"time"
+
+	"github.com/go-ping/ping"
 )
 
 const url = "http://10.10.0.251:8002/?zone=cp_htl"
@@ -23,19 +24,18 @@ func main() {
 	if err != nil {
 		fmt.Println("File not found!")
 	} else {
-		fmt.Println(b)
+		fmt.Println(string(b))
 	}
-	fmt.Println(string(b))
+
 	for {
-		start := time.Now()
-		_, err := net.LookupIP("8.8.8.8")
-		if err != nil {
-			fmt.Println("Connection timed out")
+		rtt, pktLoss := pingRun("www.google.com")
+
+		if pktLoss == float64(1) {
+			fmt.Println("Connection lost. Reconnecting...")
 			connect()
+		} else {
+			fmt.Printf("RTT: %v\n", rtt)
 		}
-		end := time.Now()
-		fmt.Println(end.Sub(start))
-		time.Sleep(1 * time.Second)
 	}
 }
 
@@ -45,6 +45,24 @@ type PostObject struct {
 	Accept   string `json:"accpe"`
 }
 
+func pingRun(host string) (rtt time.Duration, pktLoss float64) {
+	pinger, pingerErr := ping.NewPinger(host)
+	if pingerErr != nil {
+		fmt.Println(pingerErr)
+		panic("Could not create new Pinger to 'www.google.com'")
+	}
+
+	pinger.SetPrivileged(true)
+	pinger.Count = 4
+	pingerRunErr := pinger.Run()
+	if pingerRunErr != nil {
+		panic("Could not run pinger for some reason. Please try again.")
+	}
+
+	stats := pinger.Statistics()
+	return stats.AvgRtt, stats.PacketLoss
+}
+
 func connect() {
 	const username = ""
 	const password = ""
@@ -52,6 +70,7 @@ func connect() {
 	if err != nil {
 		return
 	}
-	resp, _ := http.Post(url, "application/JSON", bytes.NewBuffer(b))
+	resp, postErr := http.Post(url, "application/JSON", bytes.NewBuffer(b))
 	fmt.Println(resp)
+	fmt.Println(postErr)
 }
