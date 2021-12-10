6818246fec962a1447f7ebb4afb3e9b128c7295e
h0p3zZ <danielwimmer08012003@gmail.com>
2021-12-10 23:41:14 +0100

    changed commented code from windows to linux

--- net_windows.go
+++ net_windows.go
@@ -4,21 +4,23 @@ package main
 
 import (
 	"net"
+
+	"golang.zx2c4.com/wireguard/windows/tunnel/winipcfg"
 )
 
 func getDialInterfaceAddress() net.Addr {
-	// addrs, _ := winipcfg.GetAdaptersAddresses(winipcfg.AddressFamily(2), winipcfg.GAAFlagIncludeAll)
+	addrs, _ := winipcfg.GetAdaptersAddresses(winipcfg.AddressFamily(2), winipcfg.GAAFlagIncludeAll)
 
-	// for _, addr := range addrs {
-	// 	if addr.DNSSuffix() == "htl.grieskirchen.local" {
-	// 		ifaces, _ := net.Interfaces()
-	// 		for _, i := range ifaces {
-	// 			if addr.IfIndex == uint32(i.Index) {
-	// 				localAddr, _ := i.Addrs()
-	// 				return localAddr[0]
-	// 			}
-	// 		}
-	// 	}
-	// }
+	for _, addr := range addrs {
+		if addr.DNSSuffix() == "htl.grieskirchen.local" {
+			ifaces, _ := net.Interfaces()
+			for _, i := range ifaces {
+				if addr.IfIndex == uint32(i.Index) {
+					localAddr, _ := i.Addrs()
+					return localAddr[0]
+				}
+			}
+		}
+	}
 	return nil
 }
